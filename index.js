2d797c401fdf57f2568749e099942328ca70ed59
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-01-14 20:57:23 -0500

    feat: logic organnized into a function

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function countFroyoFlavors(str) {
+  let flavorsArray = userInput.split(",");
+  console.log(flavorsArray);
+
+  // create an obj, allows user to 'observe a table listing how many of each flavor they have ordered'
+  let flavorCount = {};
+
+  // iterate through arr
+  for (let i = 0; i < flavorsArray.length; i++) {
+    // rid of extra spaces
+    const flavor = flavorsArray[i].trim();
+    // if key is present, +1 to its value
+    if (flavorCount[flavor]) {
+      flavorCount[flavor] += 1;
+    } else {
+      // if not present, initialize its value as =1
+      flavorCount[flavor] = 1;
+    }
+  }
+
+  return flavorCount;
+}
+
 const defaultUserInput = "vanilla,vanilla,vanilla,strawberry,coffee,coffee";
 
 // prompt the user with a default value
@@ -6,24 +29,7 @@ const userInput = prompt(
   defaultUserInput
 );
 
-let flavorsArray = userInput.split(",");
-console.log(flavorsArray);
-
-// create an obj, allows user to 'observe a table listing how many of each flavor they have ordered'
-let flavorCount = {};
-
-// iterate through arr
-for (let i = 0; i < flavorsArray.length; i++) {
-  // rid of extra spaces
-  const flavor = flavorsArray[i].trim();
-  // if key is present, +1 to its value
-  if (flavorCount[flavor]) {
-    flavorCount[flavor] += 1;
-  } else {
-    // if not present, initialize its value as =1
-    flavorCount[flavor] = 1;
-  }
-}
+const result = countFroyoFlavors(userInput);
 
 // console.table the obj we just built
-console.table(flavorCount);
+console.table(result);
